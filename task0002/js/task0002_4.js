5a124d1dc9f5ebda721f8b1bf5de96109584e352
22Kon <2782596816@qq.com>
2020-04-21 21:23:46 +0800

    add blog k8s installation

---
+++ task0002/js/task0002_4.js
@@ -0,0 +1,10 @@
+var choice = $('#prompt').getElementsByTagName('li');
+
+function showHint() {
+    $('#prompt').style.display = 'block';
+    console.log(choice);
+}
+
+$.click(choice, function() {
+
+});
