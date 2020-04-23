9ae9f54618fbcbc1a55215af8151dc2d3f640f14
22Kon <2782596816@qq.com>
2020-04-23 20:39:20 +0800

    ife task0002 done

--- task0002/js/task0002_4.js
+++ task0002/js/task0002_4.js
@@ -1,10 +1,18 @@
 var choice = $('#prompt').getElementsByTagName('li');
+var inputBox = $('#autoInput');
 
 function showHint() {
     $('#prompt').style.display = 'block';
-    console.log(choice);
+    // inputBox.value = 'aaa';
 }
 
-$.click(choice, function() {
+console.log(choice[1].innerText);
 
-});
+
+for (var i=0; i<choice.length; i++) {
+    $.click(choice[i], function() {
+        var index = this.getAttribute('index');
+        inputBox.value = choice[index].innerText;
+        $('#prompt').style.display = 'none';
+    });
+}
